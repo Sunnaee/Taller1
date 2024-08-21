1d8611415b716299c60208315410d4c50ced25c0
Sabrina <s.lopez15@ufromail.cl>
2024-08-21 10:25:23 -0400

    Corrección indentación y creación método obtener carta con random

--- src/main/java/juego.java
+++ src/main/java/juego.java
@@ -1,45 +1,53 @@
-//random
+import java.util.Random;
+
 public class juego {
     public static void main(String[] args){
-        String[][] mazo = inicializarCartasJuego(crearMatrizCartas());
+            String[][] mazo = inicializarCartasJuego(crearMatrizCartas());
+            for(int i=0;i<12;i++){
+                System.out.print(mazo[i][0]);
+                System.out.println(mazo[i][1]);
+            }
+    }
+    //crear Matriz de las cartas.
+    public static String[][] crearMatrizCartas(){
+        String[][] cartas = new String[12][2];
+        return cartas;
+    }
+    //integer.parseInt()
+    //agregar cartas.
+    public static String[][] agregarCartas(String[][] matriz,String nombreCarta,String puntaje){
         for(int i=0;i<12;i++){
-            System.out.print(mazo[i][0]);
-            System.out.println(mazo[i][1]);
+            if(matriz[i][0] == null){
+                matriz[i][0] = nombreCarta;
+                matriz[i][1] = puntaje;
+                break;
+            }
         }
+        return matriz;
     }
-
-
-//crear Matriz de las cartas.
-public static String[][] crearMatrizCartas(){
-    String[][] cartas = new String[12][2];
-    return cartas;
-}
-//integer.parseInt()
-//agregar cartas.
-public static String[][] agregarCartas(String[][] matriz,String nombreCarta,String puntaje){
-    for(int i=0;i<12;i++){
-        if(matriz[i][0] == null){
-            matriz[i][0] = nombreCarta;
-            matriz[i][1] = puntaje;
-            break;
-        }
+    //inicializar cartas para el juego.
+    public static String[][] inicializarCartasJuego(String[][] matriz){
+        matriz = agregarCartas(matriz,"As","11");
+        matriz = agregarCartas(matriz,"2","2");
+        matriz = agregarCartas(matriz,"3","3");
+        matriz = agregarCartas(matriz,"4","4");
+        matriz = agregarCartas(matriz,"5","5");
+        matriz = agregarCartas(matriz,"6","6");
+        matriz = agregarCartas(matriz,"7","7");
+        matriz = agregarCartas(matriz,"8","8");
+        matriz = agregarCartas(matriz,"9","9");
+        matriz = agregarCartas(matriz,"Jota","10");
+        matriz = agregarCartas(matriz,"Quina","10");
+        matriz = agregarCartas(matriz,"Kaiser","10");
+        return matriz;
     }
-    return matriz;
-}
-//inicializar cartas para el juego.
-public static String[][] inicializarCartasJuego(String[][] matriz){
-    matriz = agregarCartas(matriz,"As","11");
-    matriz = agregarCartas(matriz,"2","2");
-    matriz = agregarCartas(matriz,"3","3");
-    matriz = agregarCartas(matriz,"4","4");
-    matriz = agregarCartas(matriz,"5","5");
-    matriz = agregarCartas(matriz,"6","6");
-    matriz = agregarCartas(matriz,"7","7");
-    matriz = agregarCartas(matriz,"8","8");
-    matriz = agregarCartas(matriz,"9","9");
-    matriz = agregarCartas(matriz,"Jota","10");
-    matriz = agregarCartas(matriz,"Quina","10");
-    matriz = agregarCartas(matriz,"Kaiser","10");
-    return matriz;
-}
+    //obtener cartas
+    public static int obtenerCartas(){
+        Random numeroRandom = new Random();
+        int posicionCarta = numeroRandom.nextInt(12);
+        return posicionCarta;
+    }
+    //jugar
+    //imprimir resultado
+    //menú
 }
