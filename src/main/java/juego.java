426f81dab914da0d3af775bc524484f069f69822
Sabrina <s.lopez15@ufromail.cl>
2024-08-21 10:49:29 -0400

    Ejecución jugar

--- src/main/java/juego.java
+++ src/main/java/juego.java
@@ -2,6 +2,7 @@ import java.util.Random;
 
 public class juego {
     public static void main(String[] args){
+        jugar();
     }
     //crear Matriz de las cartas.
     public static String[][] crearMatrizCartas(){
